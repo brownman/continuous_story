e9d770c69bd0c6441462e9dd9c4178c4c533828d
brownman <ofer.shaham@gmail.com>
2013-12-16 16:05:50 +0200

    before I start

--- server/server.js
+++ server/server.js
@@ -1,36 +1,68 @@
+
 Users = new Meteor.Collection('users');
+
+Messages = new Meteor.Collection('messages');
 ChatStream = new Meteor.Stream('chat');
 
 Meteor.publish('Users', function() {
-  return Users.find();
+    return Users.find();
 });
 
+
 ChatStream.permissions.write(function() {
-  return true;
+    return true;
 });
 
 ChatStream.permissions.read(function() {
-  return true;
+    return true;
 });
 
 Meteor.methods({
-  joinRoom: function(name) {
-		var userId = Users.insert({ name: name,
-																last_keepalive: new Date().getTime() });
+    addMessage2 : function (newMessage) {
 
-		return userId;
-  },
-  keepalive: function(user_id) {
-    check(user_id, String);
+        if (newMessage.userName == "") {
+            throw new Meteor.Error(413, "Missing a user name...");
+        }
+        if (newMessage.message == "") {
+            throw new Meteor.Error(413, "Missing message content...");
+        }
+        var id = Messages.insert(newMessage);
 
-    Users.update({ _id: user_id },
-                 { $set: { last_keepalive: new Date().getTime() }});
-  }
+        var cursor = Messages.find();
+        if (cursor.count() > 20) {
+            var oldestMessage = Messages.findOne();
+            Messages.remove(oldestMessage);
+        }
+
+        return id;
+    },
+
+
+    addMessage1: function(room_name, message) {
+        Messages.insert({
+            userId: this.userId,
+        message: message,
+        created_on: new Date().getTime()
+        });
+    },
+    joinRoom: function(name) {
+        var userId = Users.insert({ name: name,
+            last_keepalive: new Date().getTime() });
+
+        return userId;
+    },
+    keepalive: function(user_id) {
+        check(user_id, String);
+
+        Users.update({ _id: user_id },
+                { $set: { last_keepalive: new Date().getTime() }});
+    }
 });
 
 Meteor.setInterval(function () {
-  var now = new Date().getTime();
-  var idle_threshold = now - 60 * 1000; // 1 minute
+    var now = new Date().getTime();
+    var idle_threshold = now - 60 * 1000; // 1 minute
 
-  Users.remove({last_keepalive: { $lt: idle_threshold }});
+    Users.remove({last_keepalive: { $lt: idle_threshold }});
 }, 30 * 1000);
+
